ca5d7bef07fc44133822613e1136fea9a13401a8
Elves Sousa <elves.sousa@gmail.com>
2020-04-09 20:44:04 -0300

    Usability improvement.

--- src/render.js
+++ src/render.js
@@ -1,29 +1,45 @@
 const videoElement = document.querySelector('video');
 const startButton = document.getElementById('start');
 const stopButton = document.getElementById('stop');
+const screenElement = document.getElementById('screen');
+const statusBar = document.getElementById('status');
 const videoSelectButton = document.getElementById('selector');
 
 const { desktopCapturer, remote } = require('electron');
 const { writeFile } = require('fs');
 const { dialog, Menu } = remote;
 
+// ----------------------------------------------------
+// Global data
+// ----------------------------------------------------
 const recordedChunks = [];
 let mediaRecorder;
 
+// ----------------------------------------------------
+// Buttons
+// ----------------------------------------------------
 startButton.onclick = (e) => {
   mediaRecorder.start();
-  startButton.classList.add('on');
-  startButton.innerText = 'Recording...';
+  startButton.classList.add('hidden');
+  stopButton.classList.remove('hidden');
+  stopButton.classList.remove('hidden');
+  statusBar.classList.remove('hidden');
+  videoSelectButton.disabled = true;
 };
 
 stopButton.onclick = (e) => {
   mediaRecorder.stop();
-  startButton.classList.remove('on');
-  startButton.innerText = 'Start';
+  startButton.classList.remove('hidden');
+  stopButton.classList.remove('hidden');
+  statusBar.classList.add('hidden');
+  videoSelectButton.disabled = false;
 };
 
 videoSelectButton.onclick = getVideoSources;
 
+// ----------------------------------------------------
+// Get available desktop windows
+// ----------------------------------------------------
 async function  getVideoSources() {
   const inputSources = await desktopCapturer.getSources({
     types: ['window', 'screen']
@@ -41,9 +57,11 @@ async function  getVideoSources() {
   videoOptionsMenu.popup();
 }
 
-
+// ----------------------------------------------------
+// Select a window from the list
+// ----------------------------------------------------
 async function selectSource(source) {
-  videoSelectButton.innerHTML = source.name;
+  screenElement.dataset.label = source.name;
   const constraints = {
     audio: false,
     video: {
@@ -61,6 +79,7 @@ async function selectSource(source) {
   // Stream on video element
   videoElement.srcObject = stream;
   videoElement.play();
+  startButton.classList.remove('hidden');
 
   // Record media from stream
   const options = { mimeType: 'video/webm; codecs=vp9' }
@@ -71,11 +90,17 @@ async function selectSource(source) {
   mediaRecorder.onstop = handleStop;
 }
 
+// ----------------------------------------------------
+// Push data to recorded chunks
+// ----------------------------------------------------
 function handleDataAvailable(e) {
   console.log('video data available');
   recordedChunks.push(e.data);
 }
 
+// ----------------------------------------------------
+// Generate buffer and save file
+// ----------------------------------------------------
 async function handleStop(e) {
   const blob = new Blob(recordedChunks, {
     type: 'video/webm; codecs=vp9'
@@ -87,6 +112,9 @@ async function handleStop(e) {
     buttonLabel: 'Save video',
     defaultPath: `video-${Date.now()}.webm`
   });
+
+  stopButton.classList.add('hidden');
+  startButton.classList.remove('hidden');
 
   console.log(filePath);
   writeFile(filePath, buffer, () => {
